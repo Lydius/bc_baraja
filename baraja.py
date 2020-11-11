33137ab7832f0d0a8ebe6fbf47c738dfdcd6ea8a
Lydia Galan Pache <lydiagalanpache@Lydias-MacBook-Pro.local>
2020-11-11 20:37:15 +0100

    Segunda versión baraja con funciones

--- baraja.py
+++ baraja.py
@@ -1,10 +1,9 @@
+
 palos = ['o','c','e','b']
 numeros = ['A','2','3','4','5','6','7','S','C','R']
 
-baraja=[]
-
 def crearBaraja():
-
+    baraja=[]
     for palo in palos:
         for numero in numeros:
             baraja.append(numero+palo)
@@ -13,6 +12,5 @@ def crearBaraja():
 
 crearBaraja()
 
-print(baraja)
 
 
